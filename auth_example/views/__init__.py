f1460608949c04779b900e3f08988433f3d13970
Linakrdozo <linakrdozo13@gmail.com>
2021-12-02 23:10:51 -0500

    last changes

--- auth_example/views/__init__.py
+++ auth_example/views/__init__.py
@@ -1,5 +1,5 @@
 from .userCreateView        import UserCreateView
 from .userDetailView        import UserDetailView, UserUpdateView
-from .accountView           import ListAccountsView
+from .accountView           import ListAccountsView, AccountCreateView
 from .prendaInventarioView  import PrendaInventarioCreateView, PrendaInventarioListView, PrendaInventarioUpdateView, PrendaInventarioDeleteView
 from .verifyTokenView       import VerifyTokenView
